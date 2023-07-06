e83105e9163f9d6c52de28be8e60b7f748230d96
Antonio Montalvo <ant.mon.diaz@hotmail.com>
2023-07-06 14:48:57 -0500

    adding search component with suggest function

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { countries } from '../models/contries';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  typeahead: FormControl = new FormControl();
+  countries: String[] = countries;
 
+  suggestion: String[] = [];
+  suggest(){
+    this.suggestion = this.countries
+    .filter(c=> c.toLowerCase().startsWith(this.typeahead.value.toLowerCase()))
+    .slice(0,5)
+  }
 }
